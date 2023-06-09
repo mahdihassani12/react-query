fca98bef55e5b9d2c55b254173ed613301e20026
Mahdi <hassanim430@gmail.com>
2023-06-09 21:30:25 +0430

    react query dev tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
 const queryClient = new QueryClient({});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,5 +12,6 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} position = 'bottom-right' />
   </QueryClientProvider>
 );
