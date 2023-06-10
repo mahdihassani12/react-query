82c8bd774e35ab7764b8cc0514016c1e535039f6
Mahdi <hassanim430@gmail.com>
2023-06-10 15:20:21 +0430

    react query polling

--- src/components/RQSuperheros.js
+++ src/components/RQSuperheros.js
@@ -14,6 +14,8 @@ function RQSuperherosPage() {
     * If the user wants to change the stale time (after how long the fetch data works in background) {staleTime: milliseconds}
     * If the user wants to fetch the api on mount: { refechOnMount: true }
     * If the user wants to keep up to date the UI with remote data: { refetchOnwindowFocus: true }
+    * If the user wants to update the data on UI in a interval: { refetchInterval: 2000 } - It only works when the user is on the screen
+    * But if the user wants keep the data update even in background { refetchIntervalInBackground: true }
     */ 
 
     if(isLoading)
